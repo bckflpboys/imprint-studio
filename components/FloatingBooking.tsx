e7c07a9fd02b6204c8fd285f1e90c90d25702230
bckflpboys <crushadde@gmail.com>
2026-01-06 08:01:49 +0200

    feat: add multi-step booking section and floating booking button

--- components/FloatingBooking.tsx
+++ components/FloatingBooking.tsx
@@ -11,9 +11,8 @@ const services = [
 ];
 
 const timeSlots = [
-    "08:00 AM", "10:00 AM", "11:00 AM", "12:00 PM",
-    "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM",
-    "05:00 PM", "06:00 PM"
+    "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM",
+    "13:00 PM", "14:00 PM", "15:00 PM", "16:00 PM", "17:00 PM"
 ];
 
 const FloatingBooking = () => {
